b525789934cea3fbeb37f90560aea7e73959e322
Olivia Michelle <omichelle@berkeley.edu>
2022-10-12 21:27:51 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      #@movies = Movie.all
+
+      @all_ratings = Movie.all_ratings()
+      if params[:ratings]
+        @ratings_to_show = params[:ratings].keys
+      else
+        @ratings_to_show = []
+      end
+      
     end
   
     def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings()
+    ['G','PG','PG-13','R']
+ end
+
+ def self.with_ratings(ratings)
+  ratings.nil? Movie.all : Movie.where(rating: ratings)
+end
+
 end
