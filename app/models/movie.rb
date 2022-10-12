af2a0091c22365e1d11e4bc6bd92f4fea1fe28f9
Olivia Michelle <omichelle@berkeley.edu>
2022-10-12 22:03:34 +0000

    4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,10 +2,14 @@ class Movie < ActiveRecord::Base
 
   def self.all_ratings
     ['G','PG','PG-13','R']
- end
-
- def self.with_ratings(ratings)
-  ratings.nil? Movie.all : Movie.where(rating: ratings)
- end
- 
+  end
+  
+  def self.with_ratings(ratings)
+    if ratings.nil?
+      Movie.all
+    else
+      Movie.where(rating: ratings)
+    end
+  end
+  
 end
